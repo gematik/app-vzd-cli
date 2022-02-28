cb65be627e55775fe744334150b472bcab904784
spilikin <108782+spilikin@users.noreply.github.com>
2022-02-28 19:44:56 +0100

    Add --ignore option to admin add-cert (Ignore error if certificate already exists)

--- vzd-cli/build.gradle.kts
+++ vzd-cli/build.gradle.kts
@@ -1,5 +1,5 @@
 val ktorVersion = "2.0.0-beta-1"
-version = "0.10.2-beta"
+version = "0.10.3-beta"
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
--- vzd-cli/src/main/kotlin/vzd/admin/cli/AddCertCommand.kt
+++ vzd-cli/src/main/kotlin/vzd/admin/cli/AddCertCommand.kt
@@ -5,13 +5,17 @@ import com.github.ajalt.clikt.core.CliktError
 import com.github.ajalt.clikt.core.requireObject
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.multiple
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.path
+import io.ktor.http.*
 import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
 import net.mamoe.yamlkt.Yaml
 import org.bouncycastle.util.encoders.Base64
 import vzd.admin.client.CertificateDataDER
 import vzd.admin.client.UserCertificate
+import vzd.admin.client.VZDResponseException
 import vzd.admin.client.toCertificateInfo
 import kotlin.io.path.inputStream
 
@@ -20,6 +24,7 @@ class AddCertCommand : CliktCommand(name = "add-cert", help = "Add certificate t
 
     //val inputFormat by option("--inform", "-i").choice("der", "pem")
     private val files by argument().path(mustBeReadable = true).multiple()
+    private val ignore by option("--ignore", "-i", help = "Ignore Error 409 (certificate exists).").flag()
 
 
     private val context by requireObject<CommandContext>()
@@ -37,9 +42,17 @@ class AddCertCommand : CliktCommand(name = "add-cert", help = "Add certificate t
             entries?.first()?.let {
                 logger.info { "Found matching Entry: ${it.directoryEntryBase.dn?.uid} ${it.directoryEntryBase.displayName}" }
                 runBlocking {
-                    context.client.addDirectoryEntryCertificate(it.directoryEntryBase.dn?.uid!!,
-                        userCertificate)
-                }
+                    try {
+                        context.client.addDirectoryEntryCertificate(it.directoryEntryBase.dn?.uid!!,
+                            userCertificate)
+                    } catch (e: VZDResponseException) {
+                        if (!ignore || e.response.status != HttpStatusCode.Conflict) {
+                            throw e
+                        }
+                        logger.warn { "Certificate with serialNumber=${userCertificate.userCertificate?.toCertificateInfo()?.serialNumber} already exists. Ignoring conflict." }
+                    }
+
+            }
             }
                 ?: run { throw CliktError("Entry with telematikID ${certificateInfo.admissionStatement.registrationNumber} not found.") }
         }
