e9a45fbd38bcb817a3a413027d87784168b794b1
spilikin <108782+spilikin@users.noreply.github.com>
2023-03-10 07:01:15 +0100

    Fix Smartcard bugs

--- directory-lib/src/main/kotlin/de/gematik/ti/directory/elaborate/SmartcardsExtension.kt
+++ directory-lib/src/main/kotlin/de/gematik/ti/directory/elaborate/SmartcardsExtension.kt
@@ -1,7 +1,9 @@
 package de.gematik.ti.directory.elaborate
 
 import de.gematik.ti.directory.admin.DirectoryEntry
+import de.gematik.ti.directory.admin.UserCertificate
 import de.gematik.ti.directory.pki.CertificateInfo
+import kotlin.math.abs
 
 private fun infereSmartcardFrom(entry: DirectoryEntry, cert1: CertificateInfo, cert2: CertificateInfo? = null): Smartcard {
     val smartcardType = if (cert2 != null && entry.directoryEntryBase.personalEntry == true) {
@@ -14,10 +16,22 @@ private fun infereSmartcardFrom(entry: DirectoryEntry, cert1: CertificateInfo, c
         SmartcardType.SMCB
     }
 
+    val notBefore = if (cert2 != null && cert2.notBefore < cert1.notBefore) {
+        cert2.notBefore
+    } else {
+        cert1.notBefore
+    }
+
+    val notAfter = if (cert2 != null && cert2.notAfter > cert1.notAfter) {
+        cert2.notAfter
+    } else {
+        cert1.notAfter
+    }
+
     return Smartcard(
         type = smartcardType,
-        notBefore = cert1.notBefore,
-        notAfter = cert1.notAfter,
+        notBefore = notBefore,
+        notAfter = notAfter,
         active = entry.userCertificates?.first { it.userCertificate?.certificateInfo?.serialNumber == cert1.serialNumber }?.active ?: false,
         certificateSerialNumbers = if (cert2 != null) listOf(cert1.serialNumber, cert2.serialNumber) else listOf(cert1.serialNumber),
     )
@@ -25,28 +39,34 @@ private fun infereSmartcardFrom(entry: DirectoryEntry, cert1: CertificateInfo, c
 
 fun DirectoryEntry.infereSmartcards(): List<Smartcard>? {
     val entry = this
-    entry.userCertificates = entry.userCertificates
-        ?.filter { it.userCertificate != null }
-        ?.sortedBy { it.notBefore }
-    return entry.userCertificates
-        ?.map { it.userCertificate?.certificateInfo }
-        ?.let {
-            buildList<Smartcard> {
-                var cert1: CertificateInfo? = null
-                it.forEach { certInfo ->
-                    if (cert1 == null) {
-                        cert1 = certInfo
-                    } else if (certInfo != null && certInfo.publicKeyAlgorithm != cert1?.publicKeyAlgorithm) {
-                        add(infereSmartcardFrom(entry, cert1!!, certInfo))
-                        cert1 = null
-                    } else {
-                        add(infereSmartcardFrom(entry, cert1!!))
-                        cert1 = certInfo
-                    }
-                }
-                if (cert1 != null) {
-                    add(infereSmartcardFrom(entry, cert1!!))
-                }
+
+    val certs = entry.userCertificates?.mapNotNull { it.userCertificate?.certificateInfo }?.sortedBy { it.notBefore } ?: emptyList()
+
+    val certPairs = identifyCertificatePairs(certs)
+
+    return certPairs.map { infereSmartcardFrom(entry, it.first, it.second) }
+}
+
+/**
+ * Small algorithm to identify the pairs of certificates bases on certificate issue date (notBefore).
+ * The close ECC and RSA certificates are, they might build a pair.
+ */
+fun identifyCertificatePairs(certs: List<CertificateInfo>): List<Pair<CertificateInfo, CertificateInfo?>> {
+    val pairs = certs.filter { it.publicKeyAlgorithm == "RSA" }.sortedBy { it.notBefore }.map { Pair<CertificateInfo,CertificateInfo?>(it, null) }.toTypedArray()
+    // iterate all EC certificates and try to find the closest RSA cert to form a pair
+    certs.filter { it.publicKeyAlgorithm == "EC" }.sortedBy { it.notBefore }.forEach {eccert ->
+        var closestIndex = -1
+        var closestPair: Pair<CertificateInfo, CertificateInfo?>? = null
+        pairs.forEachIndexed { index, pair ->
+            if (closestPair == null || abs(closestPair!!.first.notBefore.epochSeconds-eccert.notBefore.epochSeconds) > abs(pair.first.notBefore.epochSeconds-eccert.notBefore.epochSeconds)) {
+                closestIndex = index
+                closestPair = pair
             }
         }
+        if (closestIndex != -1) {
+            pairs[closestIndex] = Pair(closestPair!!.first, eccert)
+        }
+    }
+
+    return pairs.toList()
 }
